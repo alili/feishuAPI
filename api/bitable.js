8f1da19efe7943fa599f81f0b704c468193b893d
alili <122499469@qq.com>
2022-11-06 19:26:22 +0800

    feat(bitable): getRecords

--- api/bitable.js
+++ api/bitable.js
@@ -6,6 +6,14 @@ const addRecords = async function (app_token, table_id, fields) {
   return res.data
 }
 
+const getRecords = async function (app_token, table_id, page_size, page_token) {
+  const url = `https://open.feishu.cn/open-apis/bitable/v1/apps/${app_token}/tables/${table_id}/records?user_id_type=user_id&page_size=${page_size}&page_token=${page_token}`
+  const res = await http.get(url)
+
+  return res.data
+}
+
 module.exports = {
   addRecords,
+  getRecords,
 }
