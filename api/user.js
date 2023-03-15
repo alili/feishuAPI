323fdb36747db2a8f46630398a047e81331d5637
wonderful <122499469@qq.com>
2023-03-16 05:05:13 +0800

    fix(user): fix update

--- api/user.js
+++ api/user.js
@@ -13,12 +13,11 @@ const getInfo = async function (user_id, user_id_type = 'user_id', department_id
 
 const update = async function (user_id, data, user_id_type = 'user_id', department_id_type = 'department_id') {
   const url = `https://open.feishu.cn/open-apis/contact/v3/users/${user_id}`
-  const res = await http.patch(url, {
+  const res = await http.patch(url, data, {
     params: {
       user_id_type,
       department_id_type,
     },
-    data,
   })
 
   return res.data
