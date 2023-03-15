301c88613ce33d7ff6f6ff2b0e74ce28837fd2b0
wonderful <122499469@qq.com>
2023-03-16 03:50:17 +0800

    feat(user): add department_id_type

--- api/user.js
+++ api/user.js
@@ -1,10 +1,11 @@
 const { http } = require('../client')
 
-const getInfo = async function (user_id, user_id_type = 'user_id') {
+const getInfo = async function (user_id, user_id_type = 'user_id', department_id_type = 'department_id') {
   const url = `https://open.feishu.cn/open-apis/contact/v3/users/${user_id}`
   const res = await http.get(url, {
     params: {
       user_id_type,
+      department_id_type,
     },
   })
   return res.data
