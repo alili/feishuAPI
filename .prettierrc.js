a1ea8693e526ec3b960e070e15d8f408da22d3ec
nemo <122499469@qq.com>
2023-10-25 14:40:48 +0800

    fix: add prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  printWidth: 120,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  htmlWhitespaceSensitivity: 'strict',
+  vueIndentScriptAndStyle: true,
+  pugWrapAttributesThreshold: 3,
+  pugAttributeSeparator: 'none',
+  pugSingleQuote: false,
+  pugSortAttributesBeginning: ['^v-if$', '^v-else$', '^v-else-if$', '^v-for$', '^:key$', '^ref$', '^v-model'],
+  pugSortAttributesEnd: ['^:', '^:disabled$', '^@', '^@click'],
+  endOfLine: 'auto',
+}
