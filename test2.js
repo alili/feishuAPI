09ba0992e9c0a714fd868741e275f3a236a912cb
nemo <122499469@qq.com>
2024-04-24 23:15:04 +0800

    2.3.0

--- test2.js
+++
@@ -1,9 +0,0 @@
-const FSDK = require('./index.js')
-
-
-;(async () =>{
-  const FAPI = await FSDK('cli_a133c336a9fbd00c', 'pqSheT2PZdemWFMBmC09vcgIhfP1sNHi')
-  
-  let res = await FAPI.message.send('e6288gb4', 'hello world')
-  console.log(`res:`, res)
-})()
